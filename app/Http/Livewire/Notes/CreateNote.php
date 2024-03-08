57f7ca18adc3cf817886a49ac94346f752930833
Michael Mervin Ruswan <202310016@student.ibik.ac.id>
2024-03-08 12:01:35 +0700

    create livewire component

---
+++ app/Http/Livewire/Notes/CreateNote.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notes;
+
+use Livewire\Component;
+
+class CreateNote extends Component
+{
+    public function render()
+    {
+        return view('livewire.notes.create-note');
+    }
+}
